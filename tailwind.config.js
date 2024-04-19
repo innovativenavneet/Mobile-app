4e252bed68b12025cd66c3d0170997309ae77139
navneet prakash <95180723+innovativenavneet@users.noreply.github.com>
2024-04-19 15:45:55 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
