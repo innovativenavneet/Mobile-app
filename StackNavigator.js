2b1316412f0ab8f8cdce31607c274f9a56e322f5
navneet prakash <95180723+innovativenavneet@users.noreply.github.com>
2024-04-24 22:02:00 +0530

    Update StackNavigator.js

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Homescreen from '../screens/Homescreen';
+import Homescreen from './Homescreen';
 
 import ProductDetailScreen from '../screens/ProductDetailScreen';
 
